63aed71a92843f505f55e17647f04e82a34d6b8c
Leon Brocard <acme@astray.com>
2021-02-14 10:48:11 +0000

    Tweak

--- video.sh
+++ video.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg  -ss 1300 -i Frozen\ \(2013\).mp4 -i black-and-white.png -lavfi scale=w=200:h=100,paletteuse,drawtext="fontfile=Roboto-Regular.ttf:fontsize=24:x=4:y=h-text_h-4:text='00\:21'" out.png -y
+ffmpeg -ss 100 -i Top_Gun.mp4 -i inky_saturated.png -frames:v 1 -lavfi scale=w=600:h=448,drawtext="fontfile=Roboto-Regular.ttf:fontsize=24:x=4:y=h-text_h-4:fontcolor=white:text='00\:21'",paletteuse out.png  -y && open out.png
